6903e75a63160285f2f095118b4b64d9b56624ce
habibur nabi <habiburnabiarafat@gmail.com>
2024-03-27 05:00:00 +0600

    minimal all product page

---
+++ src/app/(landingPage)/products/page.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const ProductsPage = () => {
+    return (
+        <section className="container grid grid-cols-5">
+            {/* filter section */}
+            <div className="col-span-1">
+                <Card>
+                    <CardTitle className="border-l-2 pl-2 text-xl font-bold">Price Range</CardTitle>
+                    <CardContent>
+                        <div className="flex justify-between">
+                            {/* TODO: shacdn checkbox with zod here */}
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+            {/* all products section */}
+            <div className="col-span-4 p-3 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+                <div className="col-span-full mb-2">
+
+                    <h1 className="text-3xl font-bold">All Products</h1>
+                    <h2>Showing 1-12 of 24 item(s)</h2>
+                    <h2 className="text-xs text-muted-foreground text-pretty">
+                        Explore the Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </h2>
+                </div>
+                <Card>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque quidem exercitationem cum reprehenderit atque id inventore ipsum repudiandae fugiat soluta! Eaque dicta voluptatibus, sapiente excepturi et quidem laborum iusto perferendis.</Card>
+            </div>
+        </section>
+    );
+};
+
+export default ProductsPage;
