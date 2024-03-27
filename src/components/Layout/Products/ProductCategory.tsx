511ff0a8e9de9c645b4b63b87182b275203efe26
habibur nabi <habiburnabiarafat@gmail.com>
2024-03-27 21:18:58 +0600

    fixed layout break issue in filtering section of products page

--- src/components/Layout/Products/ProductCategory.tsx
+++ src/components/Layout/Products/ProductCategory.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
@@ -12,23 +12,21 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { toast } from "@/components/ui/use-toast"
-import { Card, CardContent, CardTitle } from "@/components/ui/card"
+} from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { toast } from "@/components/ui/use-toast";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 
 const FormSchema = z.object({
     type: z.enum(["all", "mentions", "none"], {
         required_error: "You need to select a notification type.",
     }),
-})
+});
 
 export function ProductCategory() {
-
-
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
-    })
+    });
 
     function onSubmit(data: z.infer<typeof FormSchema>) {
         toast({
@@ -38,65 +36,68 @@ export function ProductCategory() {
                     <code className="text-white">{JSON.stringify(data, null, 2)}</code>
                 </pre>
             ),
-        })
+        });
         // TODO: how to get radio value without btn submit? fix this
-
     }
 
-    return (<Card className='p-2 w-fit'>
-        <CardTitle className='border-l-4 hover:border-l-blue-200 transition-all pl-1 text-xl font-bold'>
-            Product Category
-        </CardTitle>
-        <CardContent className='mt-2 p-2'>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-                    <FormField
-                        control={form.control}
-                        name="type"
-                        render={({ field }) => (
-                            <FormItem className="space-y-3">
-                                <FormLabel>Notify me about...</FormLabel>
-                                <FormControl>
-                                    <RadioGroup
-                                        onValueChange={field.onChange}
-                                        defaultValue={field.value}
-                                        className="flex flex-col space-y-1"
-                                    >
-                                        <FormItem className="flex items-center space-x-3 space-y-0">
-                                            <FormControl>
-                                                <RadioGroupItem className="hidden " value="all" />
-                                            </FormControl>
-                                            <FormLabel className="font-normal cursor-pointer">
-                                                All new messages
-                                            </FormLabel>
-                                        </FormItem>
-                                        <FormItem className="flex items-center space-x-3 space-y-0">
-                                            <FormControl>
-                                                <RadioGroupItem
-                                                    className="hidden " value="mentions" />
-                                            </FormControl>
-                                            <FormLabel className="font-normal cursor-pointer">
-                                                Direct messages and mentions
-                                            </FormLabel>
-                                        </FormItem>
-                                        <FormItem className="flex items-center space-x-3 space-y-0">
-                                            <FormControl>
-                                                <RadioGroupItem
-                                                    className="hidden "
-                                                    value="none" />
-                                            </FormControl>
-                                            <FormLabel className="font-normal cursor-pointer">
-                                                Nothing</FormLabel>
-                                        </FormItem>
-                                    </RadioGroup>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </form>
-            </Form>
-        </CardContent>
-    </Card>
-    )
+    return (
+        <Card className="p-2 max-w-56 w-full">
+            <CardTitle className="border-l-4 hover:border-l-blue-200 transition-all pl-1 text-xl font-bold">
+                Product Category
+            </CardTitle>
+            <CardContent className="mt-2 p-2">
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 w-full"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="type"
+                            render={({ field }) => (
+                                <FormItem className="space-y-3">
+                                    <FormControl>
+                                        <RadioGroup
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                            className="flex flex-col space-y-1"
+                                        >
+                                            <FormItem className="flex items-center space-x-3 space-y-0">
+                                                <FormControl>
+                                                    <RadioGroupItem className="hidden " value="all" />
+                                                </FormControl>
+                                                <FormLabel className="font-normal cursor-pointer">
+                                                    All new messages
+                                                </FormLabel>
+                                            </FormItem>
+                                            <FormItem className="flex items-center space-x-3 space-y-0">
+                                                <FormControl>
+                                                    <RadioGroupItem
+                                                        className="hidden "
+                                                        value="mentions"
+                                                    />
+                                                </FormControl>
+                                                <FormLabel className="font-normal cursor-pointer">
+                                                    Direct messages and mentions
+                                                </FormLabel>
+                                            </FormItem>
+                                            <FormItem className="flex items-center space-x-3 space-y-0">
+                                                <FormControl>
+                                                    <RadioGroupItem className="hidden " value="none" />
+                                                </FormControl>
+                                                <FormLabel className="font-normal cursor-pointer">
+                                                    Nothing
+                                                </FormLabel>
+                                            </FormItem>
+                                        </RadioGroup>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    );
 }
--- src/components/Layout/Products/ProductFilter.tsx
+++ src/components/Layout/Products/ProductFilter.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
     Form,
     FormControl,
@@ -13,100 +13,105 @@ import {
     FormField,
     FormItem,
     FormLabel,
-    FormMessage
-} from '@/components/ui/form'
-import { toast } from '@/components/ui/use-toast'
+    FormMessage,
+} from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
 
-import { Card, CardContent, CardTitle } from '@/components/ui/card'
-import { IconFilter, IconFilterCode, IconFilterDollar, IconFilterSearch } from '@tabler/icons-react'
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import {
+    IconFilter,
+    IconFilterCode,
+    IconFilterDollar,
+    IconFilterSearch,
+} from "@tabler/icons-react";
 const items = [
     {
-        id: '20-50',
-        label: '$20.00 - $50.00'
+        id: "20-50",
+        label: "$20.00 - $50.00",
     },
     {
-        id: '51-100',
-        label: '$51.00 - $100.00'
+        id: "51-100",
+        label: "$51.00 - $100.00",
     },
     {
-        id: '100-200',
-        label: '$100.00 - $200.00'
+        id: "100-200",
+        label: "$100.00 - $200.00",
     },
     {
-        id: '200-500',
-        label: '$200.00 - $500.00'
-    }
-] as const
+        id: "200-500",
+        label: "$200.00 - $500.00",
+    },
+] as const;
 const FormSchema = z.object({
-    items: z.array(z.string()).refine(value => value.some(item => item), {
-        message: 'You have to select at least one item.'
-    })
-})
+    items: z.array(z.string()).refine((value) => value.some((item) => item), {
+        message: "You have to select at least one item.",
+    }),
+});
 const ProductFilter = () => {
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            items: ['20-50', '51-100']
-        }
-    })
+            items: ["20-50", "51-100"],
+        },
+    });
     const onSubmit = (data: z.infer<typeof FormSchema>) => {
         toast({
-            title: 'You submitted the following values:',
+            title: "You submitted the following values:",
             description: (
-                <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
-                    <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
                 </pre>
-            )
-        })
-        console.log(data)
-    }
+            ),
+        });
+        console.log(data);
+    };
 
     return (
-        <Card className='p-2 w-fit'>
-            <CardTitle className='border-l-4 hover:border-l-blue-200 transition-all pl-1 text-xl font-bold'>
+        <Card className="p-2 max-w-56 w-full">
+            <CardTitle className="border-l-4 hover:border-l-blue-200 transition-all pl-1 text-xl font-bold">
                 Price Range
             </CardTitle>
-            <CardContent className='mt-2 p-2'>
+            <CardContent className="mt-2 p-2">
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 w-full'>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 w-full"
+                    >
                         <FormField
                             control={form.control}
-                            name='items'
+                            name="items"
                             render={() => (
                                 <FormItem>
-                                    {items.map(item => (
+                                    {items.map((item) => (
                                         <FormField
                                             key={item.id}
                                             control={form.control}
-                                            name='items'
+                                            name="items"
                                             render={({ field }) => {
                                                 return (
                                                     <FormItem
                                                         key={item.id}
-                                                        className='flex flex-row items-center space-x-2 space-y-0'
+                                                        className="flex flex-row items-center space-x-2 space-y-0"
                                                     >
                                                         <FormControl>
                                                             <Checkbox
                                                                 checked={field.value?.includes(item.id)}
-                                                                onCheckedChange={checked => {
+                                                                onCheckedChange={(checked) => {
                                                                     return checked
-                                                                        ? field.onChange([
-                                                                            ...field.value,
-                                                                            item.id
-                                                                        ])
+                                                                        ? field.onChange([...field.value, item.id])
                                                                         : field.onChange(
                                                                             field.value?.filter(
-                                                                                value => value !== item.id
+                                                                                (value) => value !== item.id
                                                                             )
-                                                                        )
+                                                                        );
                                                                 }}
                                                             />
                                                         </FormControl>
-                                                        <FormLabel className='text-sm font-normal'>
+                                                        <FormLabel className="text-sm font-normal">
                                                             {item.label}
                                                         </FormLabel>
                                                     </FormItem>
-                                                )
+                                                );
                                             }}
                                         />
                                     ))}
@@ -114,12 +119,14 @@ const ProductFilter = () => {
                                 </FormItem>
                             )}
                         />
-                        <Button className='w-full' type='submit'>Filter <IconFilterSearch className=' size-4 ml-1' /></Button>
+                        <Button className="w-full" type="submit">
+                            Filter <IconFilterSearch className=" size-4 ml-1" />
+                        </Button>
                     </form>
                 </Form>
             </CardContent>
         </Card>
-    )
-}
+    );
+};
 
-export default ProductFilter
+export default ProductFilter;
